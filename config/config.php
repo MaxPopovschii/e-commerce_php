5a84019a10734969ef3e9dfd64aa8e7f40a04b00
MaxPopovschii <122737792+MaxPopovschii@users.noreply.github.com>
2025-09-29 15:34:19 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'db_host' => '192.168.1.100',
-    'db_user' => 'max',
-    'db_pass' => 'Dom200598!',
-    'db_name' => 'ecofootprint'
+    'db_host' => 'db_host',
+    'db_user' => 'user',
+    'db_pass' => 'password',
+    'db_name' => 'name'
 ];
